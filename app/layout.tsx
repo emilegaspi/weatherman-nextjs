cf73d0806f265758a94e2cd6b1c69b3984b1b2a8
Emi <elegaspi@juliusspencer.co.nz>
2024-03-04 21:46:48 +1300

    - setup fonts and moved global styles to ui directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import '@/app/ui/globals.css'
+import { inter } from '@/app/ui/fonts'
+import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: "Create Next App",
---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+/**
+ * @desc This file is used to keep the fonts that will be used throughout the application.
+ */
+
+import { Inter } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
